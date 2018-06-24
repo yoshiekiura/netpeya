b5efb356a8df2f2becb1fac3765ef1ca0b8ebfb8
ati2ude <ati2ude1@gmail.com>
2018-06-24 22:22:44 +0200

    save

--- apps/wallet/application/views/transactions/index.php
+++ apps/wallet/application/views/transactions/index.php
@@ -47,7 +47,7 @@
                 <tbody>
                     <?php foreach($transactions as $trans): ?>
                         <tr>
-                            <td><img class="icon" src="/assets/images/icons/<?= $trans->transaction_type ?>.png" /><?= $trans->transaction_type ?></td>
+                            <td><img class="icon" src="/assets/images/icons/<?= strtolower($trans->transaction_type) ?>.png" /><?= $trans->transaction_type ?></td>
                             <td class="fade-text"><?= $trans->date ?></td>
                             <td class="amount"><?= $user->currency_simbol . number_format($trans->amount, 2, '.', ',') ?></td>
                             <td class="fade-text"><?= $trans->status ?></td>
